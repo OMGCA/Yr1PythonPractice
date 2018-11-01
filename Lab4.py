4293a2bdf542fc3fcc4087b75805beee51318a1b
t.seb.xia <41295316+OMGCA@users.noreply.github.com>
2018-11-01 21:17:38 +0000

    All exercise

--- Lab4.py
+++ Lab4.py
@@ -1,6 +1,6 @@
 def arrDisplay(arr):
     print("[ ",end='')
-    for i in range (0,len(arr)-2):
+    for i in range (0,len(arr)-1):
         print(str(arr[i]) + " , ",end='')
     print(str(arr[len(arr)-1]) + " ]")
 
@@ -20,4 +20,40 @@ def insertAvg():
         newArr[i*2+1] = (arr[i] + arr[i+1]) / 2
     arrDisplay(newArr)
 
-insertAvg()
+def binaryToDecimal(arr):
+    decimalNumber = 0
+    for i in range (0,len(arr)):
+        decimalNumber += arr[i] * pow(2,len(arr)-i-1)
+    
+    return decimalNumber
+
+def arrayMerge(arrA,arrB):
+    newArr = [0] * (len(arrA) + len(arrB))
+    print(str(len(newArr)))
+    a = 0
+    b = 0
+    for i in range (0,len(newArr)):
+        if i % 2 == 0:
+            newArr[i] = arrA[a]
+            a+=1
+        else:
+            newArr[i] = arrB[b]
+            b+=1
+
+    return newArr
+
+def rotateArr(arr):
+    mode = input(" ")
+    if int(mode) == 1:
+        arr[0][0],arr[1][0] = arr[1][0],arr[0][0]
+        arr[1][0],arr[0][1] = arr[0][1],arr[1][0]
+        arr[1][0],arr[1][1] = arr[1][1],arr[1][0]
+    elif int(mode) == 2:
+        arr[0][0],arr[0][1] = arr[0][1],arr[0][0]
+    elif int(mode) == 3:
+        arr[0][0],arr[1][0] = arr[1][0],arr[0][0]
+        arr[0][1],arr[1][1] = arr[1][1],arr[0][1]
+    
+    return arr
+        
+print(rotateArr([[7,4],[3,2]]))
